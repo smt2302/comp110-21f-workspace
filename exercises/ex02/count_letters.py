7a3ce58d8e89ef70903381617b10f98020eb4746
Sarah Trainor <smt23@email.unc.edu>
2021-09-05 21:35:17 -0400

    test

--- exercises/ex02/count_letters.py
+++
@@ -1,6 +0,0 @@
-"""Counting letters in a string."""
-
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
-
-
-# Begin your solution here...
--- exercises/ex02/repeat_beat.py
+++
@@ -1,6 +0,0 @@
-"""Repeating a beat in a loop."""
-
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
-
-
-# Begin your solution here...
