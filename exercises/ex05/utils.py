f63dc46f3648fcbc679de105e81d827e58a4f62e
Sarah Trainor <smt23@email.unc.edu>
2021-10-07 11:12:05 -0400

    Ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,41 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730389688"
 
-# Define your functions below
+
+def only_evens(bean: list[int]) -> list[int]:
+    """Evens gang only!!"""
+    i: int = 0
+    the_evens: list[int] = list()
+    while i < len(bean):
+        if bean[i] % 2 == 0:
+            the_evens.append(bean[i])
+        i += 1
+    return the_evens
+
+
+def sub(fajita: list[int], index: int, last_index: int) -> list[int]:
+    """Ninki Minjaj."""
+    a_list: list[int] = list()
+    i: int = index
+    if i < 0:
+        i = 0
+    while i < len(fajita):
+        if i >= index and i < last_index:
+            a_list.append(fajita[i])
+        i += 1
+    return a_list
+
+
+def concat(asparagus: list[int], peanuts: list[int]) -> list[int]:
+    """Miss Camraderie."""
+    i: int = 0
+    j: int = 0
+    total: list[int] = list()
+    while i < len(asparagus):
+        total.append(asparagus[i])
+        i += 1
+    while j < len(peanuts):
+        total.append(peanuts[j])
+        j += 1
+    return total
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,70 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+__author__ = "730389688"
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub
+from exercises.ex05.utils import concat
+
+
+def test_only_evens_both() -> None:
+    """Evens and odds test."""
+    bean: list[int] = [1, 2, 3, 4, 5]
+    assert only_evens(bean) == [2, 4]
+
+
+def test_only_evens_none() -> None:
+    """List empty test."""
+    bean: list[int] = []
+    assert only_evens(bean) == []
+
+
+def test_only_evens_odds() -> None:
+    """Only odds test."""
+    bean: list[int] = [1, 3, 5]
+    assert only_evens(bean) == []
+
+
+def test_sub_normal() -> None:
+    """First one of sub."""
+    fajita: list[int] = [10, 20, 30, 40]
+    index: int = 1
+    last_index: int = 3
+    assert sub(fajita, index, last_index) == [20, 30]
+
+
+def test_sub_empty_list() -> None:
+    """Nothing is in the list."""
+    fajita: list[int] = []
+    index: int = 0
+    last_index: int = 1
+    assert sub(fajita, index, last_index)
+
+
+def test_sub_negitive() -> None:
+    """Not sure what I am doing love."""
+    fajita: list[int] = [-1, -2, 4, 5]
+    index: int = -1
+    last_index: int = 3
+    assert sub(fajita, index, last_index)
+
+
+def test_concat_one() -> None:
+    """Two normal lists."""
+    asparagus: list[int] = [1, 2, 3, 4, 5]
+    peanuts: list[int] = [6, 7, 8, 9]
+    assert concat(asparagus, peanuts) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_concat_two() -> None:
+    """One list is empty!"""
+    asparagus: list[int] = [1, 2, 3]
+    peanuts: list[int] = []
+    assert concat(asparagus, peanuts) == [1, 2, 3]
+
+
+def test_concat_three() -> None:
+    """Repeated numbers!"""
+    asparagus: list[int] = [1, 2, 3]
+    peanuts: list[int] = [1, 2, 3]
+    assert concat(asparagus, peanuts) == [1, 2, 3, 1, 2, 3]
