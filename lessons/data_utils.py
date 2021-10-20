ed2d56093e15b5ea6e88d1ae0dadbea6423ab2ef
Sarah Trainor <smt23@email.unc.edu>
2021-10-20 16:02:42 -0400

    LS20-21

---
+++ lessons/data_utils.py
@@ -0,0 +1,42 @@
+"""Some helpful utility functions for working with CSV files. LS21"""
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    # Open a handle to a data file
+    file_handle = open(filename, "r", encoding="utf8")
+    # Read that file.
+
+    # Prepare to read the data file as a CSV rather than just strings
+    csv_reader = DictReader(file_handle)
+    
+    # Read each row of the CSV line by line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when we are done, to free its resources.
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column"""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row oriented table to a coulmn oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
