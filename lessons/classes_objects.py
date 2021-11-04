3950ef47dd3cd5f5c1a6a480af33bb376c3cdc8a
Sarah Trainor <smt23@email.unc.edu>
2021-11-04 11:22:47 -0400

    LS23

---
+++ lessons/classes_objects.py
@@ -0,0 +1,45 @@
+"""Ex of a class and object instantiation."""
+
+
+class Pizza:
+    """Models the idea of a Pizza."""
+
+    # Attribute definition
+    size: str = "small"
+    toppings: int = 0
+    extra_cheese: bool = False
+
+    def __init__(self, size: str, toppings: int):
+        """Constructor definition initialization of the object."""
+        self.size = size
+        self.toppings = toppings
+
+    def price(self, tax: float) -> float:
+        """Calculate price of Pizza."""
+        total: float = 0.0
+        if self.size == "large":
+            total += 10.0
+        else:
+            total += 8.0
+
+        total += self.toppings * 0.75
+
+        if self.extra_cheese:
+            total += 1.0
+
+        total *= tax
+
+        return total
+
+
+a_pizza: Pizza = Pizza("large", 3)
+print(Pizza)
+print(a_pizza)
+print(a_pizza.size)
+print(f"Price: ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+another_pizza.price(1.05)
+print(another_pizza)
+print(f" Price: ${another_pizza.price(1.05)}")
